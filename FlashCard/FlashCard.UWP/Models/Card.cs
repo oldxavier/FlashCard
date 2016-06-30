cf5c3981eb4df520f3fe8704ac61f6c4ae602010
oldxavier <konig.balazs@hotmail.hu>
2016-06-30 20:00:30 +0200

    Created the solution and the Card class.

---
+++ FlashCard/FlashCard.UWP/Models/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashCard.UWP.Models
+{
+    public class Card
+    {
+        public string Question { get; set; }
+        public string Answer { get; set; }
+
+        public Card() { }
+        public Card(string question, string answer) : this()
+        {
+            this.Question = question;
+            this.Answer = answer;
+        }
+    }
+}
