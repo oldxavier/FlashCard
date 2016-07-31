c4d40b861efe50a616753e472281c36cfb267823
KonigTibor <konig.tibor@hotmail.hu>
2016-07-31 18:55:10 +0200

    Added the ViewModels folder and the MainPageViewModel viewmodel. Moved the viewmodel initialization code from Mainpage.xaml.cs. Modified MainPage.xaml to point to the new item source.

---
+++ FlashCard/FlashCard.UWP/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using FlashCard.UWP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashCard.UWP.ViewModels
+{
+    public class MainPageViewModel
+    {
+        public List<CardCollection> Collections { get; set; }
+
+        public MainPageViewModel()
+        {
+            this.Collections = new List<Models.CardCollection>();
+            InitializeCollections();
+        }
+
+        private void InitializeCollections()
+        {
+            this.Collections.Add(
+                new CardCollection("Kepler-törvények"));
+
+            this.Collections[0].Cards.Add(
+                new Card(
+                    "Kepler első törvénye",
+                    "A bolygók pályája ellipszis, és annak egyik gyújtópontjában van a Nap."));
+            this.Collections[0].Cards.Add(
+                new Card(
+                    "Kepler második törvénye",
+                    "A bolygók vezérsugara (a bolygót a Nappal összekötő szakasz) azonos idő alatt azonos területet súrol."));
+            this.Collections[0].Cards.Add(
+                new Card(
+                    "Kepler harmadik törvénye",
+                    "A bolygók Naptól való átlagos távolságainak (a, a pálya fél nagytengelyeinek) köbei úgy aránylanak egymáshoz, mint a keringési idejük (T) négyzetei."));
+        }
+    }
+}
